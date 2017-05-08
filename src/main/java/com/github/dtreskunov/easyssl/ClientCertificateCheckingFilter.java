af9a2338a2392e0d52bf6cf0dee0624ba38d71c0
Denis Treskunov <dtreskunov@tableausoftware.com>
2017-05-08 14:23:14 -0700

    Made ClientCertificateCheckingFilter more secure
    Unchecked exceptions thrown by X509TrustManager should now result in
    HTTP 500 Internal Server Error. Closes #2.

--- src/main/java/com/github/dtreskunov/easyssl/ClientCertificateCheckingFilter.java
+++ src/main/java/com/github/dtreskunov/easyssl/ClientCertificateCheckingFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.Ssl.ClientAuth;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -20,35 +21,46 @@ import org.springframework.web.filter.GenericFilterBean;
  * A servlet filter that responds with a {@link HttpStatus#FORBIDDEN 403 Forbidden} when the provided
  * {@link X509Certificate client certificate} is not trusted according to the provided {@link X509TrustManager} - for example,
  * if the certificate has been revoked.
+ * <p><strong>If no client certificate is associated with the request, the request is considered valid.</strong>
+ * If such requests should be rejected, you must instruct the servlet container to require certificates (i.e. use Spring Boot's
+ * {@link ClientAuth#NEED}).
  * <p>
  * This class is only necessary because Spring Boot doesn't (yet) expose underlying servlet containers' CRL/OCSP features -
  * see <a href="https://github.com/spring-projects/spring-boot/issues/6171">SPRING-BOOT 6171</a>. This means that connections
  * from revoked clients will still be accepted by the server, however, request handler logic won't be reached.
  */
 public class ClientCertificateCheckingFilter extends GenericFilterBean {
-    
+
     private static final String REQUEST_ATTRIBUTE_X509_CERTIFICATE = "javax.servlet.request.X509Certificate";
     private final Logger m_log = LoggerFactory.getLogger(getClass());
     private final X509TrustManager m_trustManager;
-    
+
     public ClientCertificateCheckingFilter(X509TrustManager trustManager) {
         super();
         m_trustManager = trustManager;
     }
 
+    private void checkClientCertificate(ServletRequest request) throws CertificateException {
+        X509Certificate[] certChain = (X509Certificate[]) request.getAttribute(REQUEST_ATTRIBUTE_X509_CERTIFICATE);
+        if (certChain == null || certChain.length == 0) {
+            m_log.trace("No client certificate provided - not checking validity");
+        } else {
+            String authType = certChain[0].getPublicKey().getAlgorithm(); // should be "RSA"
+            m_trustManager.checkClientTrusted(certChain, authType);
+            m_log.trace("Request did include a trusted client certificate");
+        }
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        
+
         try {
-            m_trustManager.checkClientTrusted((X509Certificate[]) request
-                    .getAttribute(REQUEST_ATTRIBUTE_X509_CERTIFICATE), null);
+            checkClientCertificate(request);
         } catch (CertificateException e) {
-            m_log.warn("There is a problem with the client certificate: " + e.getMessage());
-            ((HttpServletResponse)response).sendError(HttpStatus.FORBIDDEN.value(), "There is a problem with the client certificate");
+            m_log.warn("Request included an untrusted client certificate: " + e.getMessage());
+            ((HttpServletResponse)response).sendError(HttpStatus.FORBIDDEN.value(), "Client certificate invalid");
             return;
-        } catch (Throwable t) {
-            m_log.error("Unable to verify certificate status (will proceed)", t);
         }
         chain.doFilter(request, response);
     }
